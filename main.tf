9c475e92dcc73971753b4d5157f032bdca054a61
463952 <subroto.mukherjee@HMCTS.NET>
2020-07-06 13:25:22 +0100

    HMIS-60

--- main.tf
+++ main.tf
@@ -1,25 +1,35 @@
-resource "azurerm_resource_group" "rg" {
+terraform {
+  required_version = ">= 0.12.0"
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  version = "=1.36.0"
+}
+
+resource "azurerm_resource_group" "hmi_apim_rg" {
   name     = "${var.prefix}-resources"
   location = var.location
+  tags     = var.tags
 }
 
 resource "azurerm_api_management" "apim_service" {
   name                = "${var.prefix}-apim-service"
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.hmi_apim_rg.name
   sku_name            = var.sku_name
   publisher_name      = "HMCTS - HMI" # Need to check with devops whether this property is required or not
-  publisher_email     = "hmiadmin@hmcts.gov.uk" # Need to check with devops whether this property is required or not
+  publisher_email     = "company@terraform.io" # Need to check with devops whether this property is required or not
 }
 
-resource "azurerm_api_management_api" "api" {
+resource "azurerm_api_management_api" "fh-hmi-api" {
   name                = "${var.prefix}-api"
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.hmi_apim_rg.name
   api_management_name = azurerm_api_management.apim_service.name
   revision            = "1"
   display_name        = "${var.prefix}-api"
   path                = "future-hearings-api"
-  protocols           = ["https", "http"]  
+  protocols           = ["https", "http"]
   description         = "<API Description>"# will add in accordance with the backend API which we are mocking.
   import {
     content_format = var.open_api_spec_content_format
@@ -28,9 +38,9 @@ resource "azurerm_api_management_api" "api" {
 }
 resource "azurerm_api_management_api_policy" "apim-hmi-api-policy" {
 
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.hmi_apim_rg.name
   api_management_name = azurerm_api_management.apim_service.name
-  api_name            = azurerm_api_management_api.api.name
+  api_name            = azurerm_api_management_api.fh-hmi-api.name
 # Set the policy here
   xml_content = file("${path.module}/template/api-policy.xml")
 
@@ -38,7 +48,7 @@ resource "azurerm_api_management_api_policy" "apim-hmi-api-policy" {
 
 resource "azurerm_api_management_product" "product" { # We can decide on a more meaninful product name
   product_id            = "${var.prefix}-product"
-  resource_group_name   = azurerm_resource_group.rg.name
+  resource_group_name   = azurerm_resource_group.hmi_apim_rg.name
   api_management_name   = azurerm_api_management.apim_service.name
   display_name          = "${var.prefix}-product"
   subscription_required = true
--- variables.tf
+++ variables.tf
@@ -1,3 +1,14 @@
+variable "environment" {}
+variable "resource_group" {}
+
+variable "tags" {
+  description = "The tags to associate with your resources."
+  default     = {
+    createdby = "Subroto.Mukherjee"
+    purpose   = "PoC"
+  }
+}
+
 variable "prefix" {
   description = "The prefix which should be used for all resources"
   default = "fh_hmi"
@@ -6,10 +17,12 @@ variable "prefix" {
 
 variable "open_api_spec_content_format" {
   description = "The format of the content from which the API Definition should be imported. Possible values are: openapi, openapi+json, openapi+json-link, openapi-link, swagger-json, swagger-link-json, wadl-link-json, wadl-xml, wsdl and wsdl-link."
+  default = "openapi+json"
 }
 
 variable "open_api_spec_content_value" {
   description = "The Content from which the API Definition should be imported. When a content_format of *-link-* is specified this must be a URL, otherwise this must be defined inline."
+  default = ""
 }
 
 variable "product" {
@@ -20,7 +33,7 @@ variable "product" {
 variable "location" {
   description = "The Azure Region in which all resources should be created."
   type    = "string"
-  default = "UK South"
+  default = "uksouth"
 }
 
 variable "env" {
@@ -30,17 +43,10 @@ variable "env" {
 
 variable "tenant_id" {
   description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
+  default = "531ff96d-0ae9-462a-8d2d-bec7c0b42082"
 }
 
 variable "sku_name" {
   type = "string"
   default = "Developer_1"  
 }
-
-# thumbprint of the SSL certificate for API gateway tests
-# This will be required if the front end client authenticates using certificate thumbprint
-#variable api_gateway_test_certificate_thumbprints {
-#  type = "list"
-  # TODO: remove default and provide environment-specific values
-#  default = []
-#}
