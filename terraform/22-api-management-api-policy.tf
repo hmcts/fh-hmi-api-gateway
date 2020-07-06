d8115b18ac16bcd72b972034ec8fd895aa351090
karanotts <nottinghamkara@gmail.com>
2020-07-06 10:30:29 +0100

    corrected wrong apimgnt res

--- terraform/22-api-management-api-policy.tf
+++ terraform/22-api-management-api-policy.tf
@@ -1,6 +1,6 @@
 resource "azurerm_api_management_api_policy" "hmi-apim-api-policy" {
   api_name            = azurerm_api_management_api.hmi-apim-api.name
-  api_management_name = azurerm_api_management_api.hmi-apim.name
+  api_management_name = azurerm_api_management.hmi-apim.name
   resource_group_name = azurerm_resource_group.hmi_apim_rg.name
 
   xml_content = <<XML
