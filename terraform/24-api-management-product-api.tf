31829b01da6881a67eb1640c654493a82a670d62
karanotts <nottinghamkara@gmail.com>
2020-07-06 11:15:46 +0100

    add api to product

---
+++ terraform/24-api-management-product-api.tf
@@ -0,0 +1,6 @@
+resource "azurerm_api_management_product_api" "example" {
+  api_name            = azurerm_api_management_api.hmi-apim-api.name
+  product_id          = azurerm_api_management_product.hmi-apim-product.product_id 
+  api_management_name = azurerm_api_management.hmi-apim.name
+  resource_group_name = azurerm_resource_group.hmi_apim_rg.name
+}
