ee1af6ba1669fb53692fcc403ee4ac692c126205
463952 <subroto.mukherjee@HMCTS.NET>
2020-07-06 15:26:35 +0100

    HMIS-60

--- variables.tf
+++ variables.tf
@@ -17,12 +17,12 @@ variable "prefix" {
 
 variable "open_api_spec_content_format" {
   description = "The format of the content from which the API Definition should be imported. Possible values are: openapi, openapi+json, openapi+json-link, openapi-link, swagger-json, swagger-link-json, wadl-link-json, wadl-xml, wsdl and wsdl-link."
-  default = "openapi+json"
+  default = "swagger-link-json"
 }
 
 variable "open_api_spec_content_value" {
   description = "The Content from which the API Definition should be imported. When a content_format of *-link-* is specified this must be a URL, otherwise this must be defined inline."
-  default = ""
+  default = "http://conferenceapi.azurewebsites.net/?format=json"
 }
 
 variable "product" {
