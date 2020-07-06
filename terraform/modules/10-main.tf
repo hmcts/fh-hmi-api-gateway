db4570b6b3004b236419f5f8105624cd69c7e5c9
karanotts <nottinghamkara@gmail.com>
2020-07-06 09:33:26 +0100

    more refactoring

--- terraform/modules/10-main.tf
+++
@@ -1,6 +0,0 @@
-module "resource-group" {
-    source              = "./resource-group"
-    resource_group      = "${var.resource_group}"
-    location            = "${var.location}"
-    tags                = "${var.tags}"
-  }
