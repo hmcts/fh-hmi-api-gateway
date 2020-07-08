ffb312ebe79ce23d783508dd750ed8a2c89ce481
karanotts <nottinghamkara@gmail.com>
2020-07-08 12:19:35 +0100

    figuring out path to file on agent

--- terraform/22-api-management-api-policy.tf
+++ terraform/22-api-management-api-policy.tf
@@ -3,5 +3,5 @@ resource "azurerm_api_management_api_policy" "hmi-apim-api-policy" {
   api_management_name = azurerm_api_management.hmi-apim.name
   resource_group_name = azurerm_resource_group.hmi_apim_rg.name
 
-  xml_content = file("${path.module}/template/api-policy.xml")
+  xml_content = file("../template/api-policy.xml")
 }
