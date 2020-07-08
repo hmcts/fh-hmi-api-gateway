96990be9c278a6d43d2a326924b5b2c53aa4cf5b
karanotts <nottinghamkara@gmail.com>
2020-07-08 14:51:16 +0100

    changed dashes to underscores in resource nicknames

--- terraform/24-api-management-product-api.tf
+++ terraform/24-api-management-product-api.tf
@@ -1,4 +1,4 @@
-resource "azurerm_api_management_product_api" "example" {
+resource "azurerm_api_management_product_api" "hmi_apim_product_api" {
   api_name            = azurerm_api_management_api.hmi-apim-api.name
   product_id          = azurerm_api_management_product.hmi-apim-product.product_id 
   api_management_name = azurerm_api_management.hmi-apim.name
