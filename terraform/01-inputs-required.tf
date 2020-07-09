5e9d49df95cb90816ea8cf1eba0e90c97f33e37c
463952 <subroto.mukherjee@HMCTS.NET>
2020-07-09 16:48:30 +0100

    HMIS-60

--- terraform/01-inputs-required.tf
+++ terraform/01-inputs-required.tf
@@ -1,22 +1,17 @@
-variable "prefix" {
-  description = "The prefix which should be used for all resources"
-  default = "fh-hmi"
-}
+variable "prefix" {}
 variable "environment" {}
 variable "location" {
   default = "uksouth"
   }
 variable "sku_name" {
   description = "Desired SKU for the API Management"
-  }
+}
 variable "open_api_spec_content_format" {
   description = "The format of the content from which the API Definition should be imported. Possible values are: openapi, openapi+json, openapi+json-link, openapi-link, swagger-json, swagger-link-json, wadl-link-json, wadl-xml, wsdl and wsdl-link."
-  default = "swagger-link-json"
-  }
+}
 variable "open_api_spec_content_value" {
   description = "The Content from which the API Definition should be imported. When a content_format of *-link-* is specified this must be a URL, otherwise this must be defined inline."
-  default = "https://raw.githubusercontent.com/hmcts/reform-api-docs/master/docs/specs/future-hearings-request-hearing.json"
-  }
+}
 variable "tags" {
   description = "The tags to associate with your resources."
   default     = {
