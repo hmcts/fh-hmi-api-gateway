1e8414699fa58d4a60003a84b0dd9b64279a8290
karanotts <nottinghamkara@gmail.com>
2020-07-15 08:20:26 +0100

    keyvault

--- terraform/11-key-vault.tf
+++ terraform/11-key-vault.tf
@@ -1,11 +1,11 @@
-resource "azurerm_key_vault" "hmi_apim_kv" {
-    name                     = "${var.prefix}-${var.environment}-kv"
-    location                 = var.location
-    resource_group_name      = azurerm_resource_group.hmi_apim_rg.name
-    tenant_id                = var.principal_tenant_id
-    sku_name                 = var.kv_sku_name
-    tags                     = var.tags
-}
+# resource "azurerm_key_vault" "hmi_apim_kv" {
+#     name                     = "${var.prefix}-${var.environment}-kv"
+#     location                 = var.location
+#     resource_group_name      = azurerm_resource_group.hmi_apim_rg.name
+#     tenant_id                = var.principal_tenant_id
+#     sku_name                 = var.kv_sku_name
+#     tags                     = var.tags
+# }
 
 # resource "azurerm_key_vault_access_policy" "permissions" {
 #     key_vault_id            = azurerm_key_vault.hmi_apim_kv.id
