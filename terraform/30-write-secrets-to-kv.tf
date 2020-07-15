d243782223a954ef790e9118f3a8af4d6ddec8c1
karanotts <nottinghamkara@gmail.com>
2020-07-15 10:12:09 +0100

    write secret to kv

--- terraform/30-write-secrets-to-kv.tf
+++ terraform/30-write-secrets-to-kv.tf
@@ -1,5 +1,5 @@
 resource "azurerm_key_vault_secret" "subscription_key" {
-  name         = azurerm_api_management.hmi_apim.name-"key"
+  name         = azurerm_api_management.hmi_apim.name
   value        = azurerm_api_management_subscription.hmi_apim_subscription.primary_key
   key_vault_id = azurerm_key_vault.hmi_apim_kv.id
   tags         = var.tags
